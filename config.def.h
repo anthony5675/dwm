5f270bd5efbe3e2a2218bc33125d6aafc803178a
Anthony Vu <anthonyvu.d@gmail.com>
2019-03-30 21:22:11 +1100

    Apply switchcol patch

--- config.def.h
+++ config.def.h
@@ -92,6 +92,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,		XK_j,	   pushdown,	   {0} },
 	{ MODKEY|ShiftMask,		XK_k,	   pushup,	   {0} },
+	{ MODKEY,                       XK_n,  	   switchcol,      {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
